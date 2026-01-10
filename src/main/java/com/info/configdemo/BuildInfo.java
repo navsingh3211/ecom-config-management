b324f9e008f97fa2cbff93f08ecfde2edc5a4d43
navsingh3211 <navsingh3211@gmail.com>
2026-01-10 21:22:35 +0530

    connecting ecom to cloud config client

--- src/main/java/com/info/configdemo/BuildInfo.java
+++ src/main/java/com/info/configdemo/BuildInfo.java
@@ -1,10 +1,14 @@
 package com.info.configdemo;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties
+@ConfigurationProperties(prefix = "build")
+@Data
 public class BuildInfo {
-    
+    private String id;
+    private String version;
+    private String name;
 }
