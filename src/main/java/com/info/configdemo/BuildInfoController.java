b8f7b596380e71706bf7ce206217c61829f32e32
navsingh3211 <navsingh3211@gmail.com>
2026-01-11 12:53:24 +0530

    comment

--- src/main/java/com/info/configdemo/BuildInfoController.java
+++ src/main/java/com/info/configdemo/BuildInfoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@RefreshScope
+@RefreshScope // to avaid downtime during config change
 public class BuildInfoController {
 
 //    @Value("${build.id:default}")
